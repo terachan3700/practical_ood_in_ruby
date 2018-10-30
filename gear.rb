153e7bbf5ca5da56ecd0fb441b9447a8bcb0ed17
terachan3700 <chapico01@gmail.com>
2018-10-30 20:59:49 +0900

    依存関係の逆転

--- gear.rb
+++ gear.rb
@@ -1,45 +1,38 @@
-module SomeFramework
 class Gear
-    attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog
 
-    def initialize(chainring, cog, wheel)
-      @chainring = chainring
-      @cog = cog
-      @wheel = wheel
-    end
-
-    def gear_inches
-      puts ratio * wheel.diameter
-    end
-
-    def ratio
-      chainring / cog.to_f
-    end
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
   end
-end
 
-module GearWrapper
-  def self.gear(args)
-    SomeFramework::Gear.new(args[:chainring],
-                            args[:cog],
-                            args[:wheel])
+  def gear_inches(diameter)
+    ratio * diameter
+  end
+
+  def ratio
+    chainring / cog.to_f
   end
 end
 
 class Wheel
-  attr_reader :rim, :tire
+  attr_reader :rim, :tire, :gear
 
-  def initialize(rim, tire)
+  def initialize(rim, tire, chainring, cog)
     @rim = rim
     @tire = tire
+    @gear = Gear.new(chainring, cog)
   end
 
   def diameter
     rim + (tire * 2)
   end
+
+  def gear_inches
+    gear.gear_inches(diameter)
+  end
+
 end
 
-GearWrapper.gear(
-  chainring: 52,
-  cog: 11,
-  wheel: Wheel.new(26, 1.5)).gear_inches
+puts Wheel.new(26, 1.5, 52, 11).gear_inches
+
