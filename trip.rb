18cdea6b131d3238fe14b1cd7adeddcf8c57d802
terachan3700 <chapico01@gmail.com>
2018-10-31 21:53:07 +0900

    あえて危ういコードに修正

--- trip.rb
+++ trip.rb
@@ -8,8 +8,37 @@ class Trip
   end
 
   # この'mechanic'引数はどんなクラスのものでも良い
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(preparers)
+
+    # 出口のないコード　危険な匂いがする
+    preparers.each do |preparer|
+      case preparer
+      when Machanic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoordinator
+        preparer.buy_food(customers)
+      when Drier
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank
+      end
+    end
+  end
+end
+
+# TripCoordinatorとDriverを追加した
+class TripCoordinator
+  def buy_food(customers)
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+  end
+
+  def fill_water_tank(vehicle)
+  end
+
+  def fill_water_tank(vehicle)
   end
 end
 
@@ -24,7 +53,7 @@ class Mechanic
   end
 end
 
-bicycle = %w(a b c)
-machin = Mechanic.new
-trip = Trip.new(bicycle)
-trip.prepare machin
+# bicycle = %w(a b c)
+# machin = Mechanic.new
+# trip = Trip.new(bicycle)
+# trip.prepare machin
