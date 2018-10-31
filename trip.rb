506e7fc4d1be06a5f64ded166fdcf36fc2983086
terachan3700 <chapico01@gmail.com>
2018-10-31 21:18:56 +0900

    ダックタイピングの理解 typo修正

--- trip.rb
+++ trip.rb
@@ -1,17 +1,30 @@
 class Trip
   attr_reader :bicycles, :customers, :vehicle
+
+  def initialize(bicycles, customers = nil, vehicle = nil)
+    @bicycles = bicycles
+    @customers = customers
+    @vehicle = vehicle
+  end
+
   # この'mechanic'引数はどんなクラスのものでも良い
   def prepare(mechanic)
-    mechanic.prepare_bycicles(bicycles)
+    mechanic.prepare_bicycles(bicycles)
   end
 end
 
 # このクラスのインスタンスを渡すことになっても動作する。
 class Mechanic
   def prepare_bicycles(bicycles)
-    bicycles.each { |bicycle| prepare_bicycles(bicycle) }
+    bicycles.each { |bicycle| prepare_bicycle(bicycle) }
   end
 
   def prepare_bicycle(bicycle)
+    puts "hello" + bicycle
   end
 end
+
+bicycle = %w(a b c)
+machin = Mechanic.new
+trip = Trip.new(bicycle)
+trip.prepare machin
