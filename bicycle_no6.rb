9537c807e3234123510e342e6c0c9fb298009a5b
terachan3700 <chapico01@gmail.com>
2018-11-06 11:37:14 +0900

    アンチパターン。styleで分岐してしまっている

--- bicycle_no6.rb
+++ bicycle_no6.rb
@@ -1,26 +1,35 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color,
+              :front_shock, :rear_shock
 
   def initialize(args)
+    @style = args[:style]
     @size = args[:size]
     @tepe_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
-  # 全ての自転車はデフォルト値として
-  # 同じタイヤサイズとチェーンサイズをもつ
+  # styleの確認は危険な道へ進む一歩
   def spares
-    { chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      { chain: '10-speed',
+        tire_size: '23',
+        tape_color: tape_color
+      }
+    else
+      { chain: '10-speed',
+        tire_size: '2.1',
+        rear_shock: rear_shock
+      }
+    end
   end
 
-  # 他にもメソッドがたくさん、、、
+  bike = Bicycle.new(
+    style: :mountain,
+    size: 'M',
+    front_shock: 'Manitou',
+    rear_shock: 'Fox')
 
-end
-
-bike = Bicycle.new(size: 'M', tape_color: 'red')
-
-puts bike.size
-puts bike.spares
+  puts bike.spares
 
