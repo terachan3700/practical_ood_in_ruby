5cff8b5401a7426a72b00b5ce3680597feeabc5d
terachan3700 <chapico01@gmail.com>
2018-11-11 20:51:32 +0900

    chapter7の最初のコード。リファクタ前

---
+++ chapter7.rb
@@ -0,0 +1,36 @@
+class Schedule
+  def scheduled?(schedulable, start_date, end_date)
+    puts "This #{schedulable.class}" +
+           "is not scheduled¥n" +
+           " between #{start_date} and #{end_date}"
+    false
+  end
+end
+
+class Bicycle
+  attr_reader :schedule, :size, :chain, :tire_size
+
+  # Scheduleを注入し、初期値を設定する。
+  def initialize(args = {})
+    @schedule = args[:schedule] || Schedule.new
+  end
+
+  def schedulable?(start_date, end_date)
+    !scheduled?(start_date - lead_days, end_date)
+  end
+
+  def scheduled?(start_date, end_date)
+    schedule.scheduled?(self, start_date, end_date)
+  end
+
+  def lead_days
+    1
+  end
+end
+
+require 'date'
+starting = Date.parse("2015/09/04")
+ending = Date.parse("2015/09/10")
+
+b = Bicycle.new
+puts b.schedulable?(starting,ending)
