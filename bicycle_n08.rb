33cae15f115e29253813b8b02e5c24ac3ccbff19
terachan3700 <chapico01@gmail.com>
2018-11-11 23:06:24 +0900

    Partクラスを作りリファクタリング

--- bicycle_n08.rb
+++ bicycle_n08.rb
@@ -12,34 +12,27 @@ class Bicycle
 end
 
 class Parts
-  attr_reader :chain, :tire_size
+  attr_reader :parts
 
-  def initialize(args = {})
-    @chain = args[:chain] || default_chain
-    @tire_size = args[:tire_size] || default_tire_size
+  def initialize(parts)
+    @parts = parts
   end
 
   def spares
-    { tire_size: tire_size,
-      chain: chain }.merge(local_spares)
-  end
-
-  def default_tire_size
-    raise NotImplementedError
-  end
-
-  def post_initialize(args)
-    nil
-  end
-
-  def local_spares
-    {}
-  end
-
-  def default_chain
-    '10-speed'
+    parts.select { |part| part.needs_spare }
   end
 end
+
+class Part
+  attr_reader :name, :description, :needs_spare
+
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:description]
+    @need_spare = args.fetch(:needs_spare, true)
+  end
+end
+
 
 class RoadBikeParts < Parts
   attr_reader :tape_color
@@ -74,16 +67,24 @@ class MounteinBikeParts < Parts
   end
 end
 
+chain = Part.new(name: 'chain', description: '10-speed')
+road_tire = Part.new(name: 'tire_size', description: '23')
+tape = Part.new(name: 'tape_color', description: 'red')
+mountain_tire = Part.new(name: 'tire_size', description: '2.1')
+rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
+front_shock = Part.new(name: 'front_shock', description: 'Manitou', needs_spare: false)
+
+
 road_bike = Bicycle.new(
   size: 'L',
-  parts: RoadBikeParts.new(tape_color: 'red'))
+  parts: Parts.new([chain, road_tire, tape]))
 
 puts road_bike.size
 puts road_bike.spares
 
 mountain_bike = Bicycle.new(
   size: 'L',
-  parts: MounteinBikeParts.new(rear_shock: 'Fox'))
+  parts: Parts.new([chain, mountain_tire,front_shock,rear_shock]))
 
 puts mountain_bike.size
 puts mountain_bike.spares
