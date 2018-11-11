a936b50e990847954eceac7d3b6672dd46ddde80
terachan3700 <chapico01@gmail.com>
2018-11-11 21:13:12 +0900

    サブクラスもmoduleをincludeすることでlead_daysだけ実装すれば良い

--- chapter7.rb
+++ chapter7.rb
@@ -18,9 +18,31 @@ class Bicycle
   end
 end
 
+class Vehicle
+  include Schedulable
+
+  def lead_days
+    3
+  end
+end
+
+class Mechanic
+  include Schedulable
+
+  def lead_days
+    4
+  end
+end
+
 require 'date'
 starting = Date.parse("2015/09/04")
 ending = Date.parse("2015/09/10")
 
 b = Bicycle.new
 puts b.schedulable?(starting,ending)
+
+v = Vehicle.new
+puts v.schedulable?(starting,ending)
+
+m = Mechanic.new
+puts m.schedulable?(starting,ending)
